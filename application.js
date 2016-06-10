da00476782ae06ddc40486b21741ac43e861b368
jhedgemond <jhedgemond@kippnyccp.org>
2016-06-10 10:30:52 -0400

    Update application.js

--- application.js
+++ application.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $("#submit").click(function(){
-    var name=$("input[name=firstname]").val();
+    var name=$("input[name=first_name]").val();
     var comment=$("input[name=comment]").val();
-    alert(comment);
+    append(comment);
   })
 })
